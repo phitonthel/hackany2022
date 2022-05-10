ed20b5b7f4ac206f431819e205fcf83790a4d556
phitonthel <thephitonthel@gmail.com>
2022-05-10 14:35:06 +0700

    add cors

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
 const routes = require('./routers')
 
 const PORT = process.env.PORT || 3000
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.use(cors())
 
 app.use(routes)
 
